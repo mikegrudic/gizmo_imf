5c28e5f5c361d487245357f4ff07f6bd2d8df8e8
Philip Hopkins <phopkins@caltech.edu>
2022-09-13 13:30:44 -0700

    fix for compiler issue with certain dust+starforge experimental flags

--- radiation/rt_source_injection.c
+++ radiation/rt_source_injection.c
@@ -213,25 +213,27 @@ int rt_sourceinjection_evaluate(int target, int mode, int *exportflag, int *expo
                         #pragma omp atomic
                         SphP[j].VelPred[kv] += dv_tmp;
                     } // applies direction and converts to code units
-#endif                    
+#endif
+#endif
+
 
 #ifdef RT_REPROCESS_INJECTED_PHOTONS // conserving photon energy, put only the un-absorbed component of the current band into that band, putting the rest in its "donation" bin (ionizing->optical, all others->IR). This would happen anyway during the routine for resolved absorption, but this may more realistically handle situations where e.g. your dust destruction front is at totally unresolved scales and you don't want to spuriously ionize stuff on larger scales. Assume isotropic re-radiation, so inject only energy for the donated bin and not net flux/momentum.
-                    double dE_donation=0; int donation_bin=rt_get_donation_target_bin(k), do_donation=0; if(donation_bin > -1){do_donation = 1;}
+                    double dE_donation=0; int donation_bin=rt_get_donation_target_bin(k), do_donation=0; if(donation_bin > -1) {do_donation = 1;}
 #ifdef RT_CHEM_PHOTOION  // figure out if we have enough photons to carve a Stromgren sphere through this cell. If yes, inject ionizing radiation, otherwise more accurate to downgrade it to model an unresolved HII region
 #if (RT_CHEM_PHOTOION==1)
-		    if(k == RT_FREQ_BIN_H0) { // just one ionizing bin
+                    if(k == RT_FREQ_BIN_H0) { // just one ionizing bin
 #else 
                     if((k >= RT_FREQ_BIN_H0) && (k < RT_FREQ_BIN_H0 + 4)) { // do this block for all ionizing bins
 #endif
                         double stellum=0;
-			if(local.Dt > 0) {
+                        if(local.Dt > 0) {
 #if (RT_CHEM_PHOTOION==1)			  
-			    stellum = local.Luminosity[RT_FREQ_BIN_H0];
+                            stellum = local.Luminosity[RT_FREQ_BIN_H0];
 #else			    
-			    int k2; for(k2=RT_FREQ_BIN_H0; k2 < RT_FREQ_BIN_H0 + 4; k2++) { stellum += local.Luminosity[k2]; } // add up total ionizing photon energy
+                            int k2; for(k2=RT_FREQ_BIN_H0; k2 < RT_FREQ_BIN_H0 + 4; k2++) {stellum += local.Luminosity[k2];} // add up total ionizing photon energy
 #endif			    
-			    stellum *= 1. / (C_LIGHT_CODE_REDUCED/C_LIGHT_CODE) / local.Dt * UNIT_LUM_IN_CGS; // convert energy to luminosity in cgs
-			}
+                            stellum *= 1. / (C_LIGHT_CODE_REDUCED/C_LIGHT_CODE) / local.Dt * UNIT_LUM_IN_CGS; // convert energy to luminosity in cgs
+                        }
                         double RHII = 4.01e-9*pow(stellum,0.333)*pow(SphP[j].Density*All.cf_a3inv*UNIT_DENSITY_IN_CGS,-0.66667) / UNIT_LENGTH_IN_CGS;
                         if(DMAX(r, Get_Particle_Size(j))*All.cf_atime < RHII) {do_donation = 0;} // don't inject ionizing photons outside the Stromgren radius
                     }
@@ -242,11 +244,10 @@ int rt_sourceinjection_evaluate(int target, int mode, int *exportflag, int *expo
                     if(do_donation) {dE_donation=slabfac_x*dE; dE *= fabs(1-slabfac_x);}
 #endif // RT_REPROCESS_INJECTED_PHOTONS
 
-#if defined(RT_EVOLVE_FLUX) /* when we use RT_INJECT_PHOTONS_DISCRETELY_ADD_MOMENTUM_FOR_LOCAL_EXTINCTION, we add the 'full' optically-thin flux directly to the neighbor cells. a more general formulation allows these fluxes to build up self-consistently, since we don't know a-priori what these 'should' be */
+#if defined(RT_EVOLVE_FLUX) && defined(RT_INJECT_PHOTONS_DISCRETELY_ADD_MOMENTUM_FOR_LOCAL_EXTINCTION) /* when we use these flags, we add the 'full' optically-thin flux directly to the neighbor cells. a more general formulation allows these fluxes to build up self-consistently, since we don't know a-priori what these 'should' be */
                     double dflux = -dE * C_LIGHT_CODE_REDUCED / r;
                     for(kv=0;kv<3;kv++) {dfluxes[kv] += dflux*dp[kv];}
 #endif
-#endif // RT_INJECT_PHOTONS_DISCRETELY_ADD_MOMENTUM_FOR_LOCAL_EXTINCTION
 
                     
 #if defined(RT_INJECT_PHOTONS_DISCRETELY)
