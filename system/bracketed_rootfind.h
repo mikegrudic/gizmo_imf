3fc126265490de379153c0dc6256633fa5255871
Mike Grudic <mgrudic@pop-os.localdomain>
2024-07-01 09:56:10 -0700

    formatting of bracketed_rootfind.h

--- system/bracketed_rootfind.h
+++ system/bracketed_rootfind.h
@@ -1,15 +1,16 @@
 /*
-Code block containing a Brent 1973 rootfind routine, guaranteed to find the root provided
-upper and lower bounds for a continuous function
+Code block containing a Brent 1973 rootfind routine, guaranteed to find the root
+provided upper and lower bounds for a continuous function
 
 Required initializations outside this block:
     ROOTFIND_FUNCTION(x) - #define'd macro that evaluates a function of a single
 variable whose root we wish to find
     ROOTFIND_X_a, ROOTFIND_X_b - Arguments to ROOTFIND_FUNCTION that are known
 to bracket the root.
-    ROOTFUNC_a, ROOTFUNC_b - values of the function evaluated at the bracket points
-    ROOTFIND_REL_X_tol - Tolerance for desired relative error in the root; stop iterating when this is achieved
-    ROOTFIND_ABS_X_tol - Tolerance for desired absolute error in the root; stop iterating when this is achieved
+    ROOTFUNC_a, ROOTFUNC_b - values of the function evaluated at the bracket
+points ROOTFIND_REL_X_tol - Tolerance for desired relative error in the root;
+stop iterating when this is achieved ROOTFIND_ABS_X_tol - Tolerance for desired
+absolute error in the root; stop iterating when this is achieved
 */
 
 // if doing nested rootfinds, need to def a different inner function because
@@ -20,13 +21,14 @@ to bracket the root.
 #define ROOTFUNC ROOTFIND_FUNCTION
 #endif
 
-if (ROOTFUNC_a * ROOTFUNC_b > 0)
-{
-    PRINT_WARNING("ERROR: Bounds supplied to bracketed_roofind.h block do not bracket the root. x_a=%g x_b=%g f_a=%g f_b=%g Expanding region...",ROOTFIND_X_a, ROOTFIND_X_b, ROOTFUNC_a,ROOTFUNC_b);
+if (ROOTFUNC_a * ROOTFUNC_b > 0) {
+    PRINT_WARNING(
+        "ERROR: Bounds supplied to bracketed_roofind.h block do not bracket "
+        "the root. x_a=%g x_b=%g f_a=%g f_b=%g Expanding region...",
+        ROOTFIND_X_a, ROOTFIND_X_b, ROOTFUNC_a, ROOTFUNC_b);
     double bracket_fac = 1.1;
     int bracket_iter = 0;
-    do
-    {
+    do {
         double tmp = ROOTFIND_X_a; // let a be the lower value
         ROOTFIND_X_a = DMIN(ROOTFIND_X_a, ROOTFIND_X_b) / bracket_fac;
         ROOTFIND_X_b = DMAX(tmp, ROOTFIND_X_b) * bracket_fac;
@@ -34,17 +36,17 @@ if (ROOTFUNC_a * ROOTFUNC_b > 0)
         ROOTFUNC_b = ROOTFUNC(ROOTFIND_X_b);
         bracket_iter++;
     } while (ROOTFUNC_a * ROOTFUNC_b > 0 && bracket_iter < MAXITER);
-    if ((bracket_iter == MAXITER) || isnan(ROOTFUNC_a) || isnan(ROOTFUNC_b))
-    {
-        PRINT_WARNING("ERROR: Could not bracket root. x_a=%g x_b=%g f_a=%g f_b=%g\n", ROOTFIND_X_a, ROOTFIND_X_b, ROOTFUNC_a, ROOTFUNC_b);
+    if ((bracket_iter == MAXITER) || isnan(ROOTFUNC_a) || isnan(ROOTFUNC_b)) {
+        PRINT_WARNING(
+            "ERROR: Could not bracket root. x_a=%g x_b=%g f_a=%g f_b=%g\n",
+            ROOTFIND_X_a, ROOTFIND_X_b, ROOTFUNC_a, ROOTFUNC_b);
         endrun(234528);
     }
 }
 
-if (fabs(ROOTFUNC_a) < fabs(ROOTFUNC_b))
-{ // in our convention 'a' represents
-  // the bracket with the larger
-  // residual
+if (fabs(ROOTFUNC_a) < fabs(ROOTFUNC_b)) { // in our convention 'a' represents
+                                           // the bracket with the larger
+                                           // residual
     double tmp = ROOTFUNC_a;
     ROOTFUNC_a = ROOTFUNC_b;
     ROOTFUNC_b = tmp;
@@ -59,21 +61,17 @@ double ROOTFIND_X_c_old = ROOTFIND_X_c, ROOTFIND_X_new,
        ROOTFUNC_new = ROOTFUNC_c, ROOTFIND_X_error = 1e100, EPS_TOL = 1e-14;
 
 /* now we do a Brent 1973 method root-find */
-do
-{
+do {
     ROOTFIND_X_new = 0;
     if ((ROOTFUNC_a != ROOTFUNC_c) &&
-        (ROOTFUNC_b != ROOTFUNC_c))
-    { // inverse quadratic interpolation
+        (ROOTFUNC_b != ROOTFUNC_c)) { // inverse quadratic interpolation
         ROOTFIND_X_new += ROOTFIND_X_a * ROOTFUNC_c * ROOTFUNC_b /
                           (ROOTFUNC_a - ROOTFUNC_b) / (ROOTFUNC_a - ROOTFUNC_c);
         ROOTFIND_X_new += ROOTFIND_X_b * ROOTFUNC_c * ROOTFUNC_a /
                           (ROOTFUNC_b - ROOTFUNC_a) / (ROOTFUNC_b - ROOTFUNC_c);
         ROOTFIND_X_new += ROOTFIND_X_c * ROOTFUNC_a * ROOTFUNC_b /
                           (ROOTFUNC_c - ROOTFUNC_a) / (ROOTFUNC_c - ROOTFUNC_b);
-    }
-    else
-    { // secant method
+    } else { // secant method
         ROOTFIND_X_new =
             (ROOTFIND_X_a * ROOTFUNC_b - ROOTFIND_X_b * ROOTFUNC_a) /
             (ROOTFUNC_b - ROOTFUNC_a);
@@ -81,82 +79,62 @@ do
     DO_BISECTION = 0;
     double ROOTFIND_X_midpoint_a = 0.25 * (3 * ROOTFIND_X_a + ROOTFIND_X_b);
     if ((ROOTFIND_X_new < DMIN(ROOTFIND_X_midpoint_a, ROOTFIND_X_b)) ||
-        (ROOTFIND_X_new > DMAX(ROOTFIND_X_midpoint_a, ROOTFIND_X_b)))
-    {
+        (ROOTFIND_X_new > DMAX(ROOTFIND_X_midpoint_a, ROOTFIND_X_b))) {
         DO_BISECTION = 1;
     }
-    if (USED_BISECTION)
-    {
+    if (USED_BISECTION) {
         if (fabs(ROOTFIND_X_new - ROOTFIND_X_b) >=
-            0.5 * fabs(ROOTFIND_X_c - ROOTFIND_X_b))
-        {
+            0.5 * fabs(ROOTFIND_X_c - ROOTFIND_X_b)) {
             DO_BISECTION = 1;
         }
-        if (ROOTFIND_X_b != ROOTFIND_X_c)
-        {
+        if (ROOTFIND_X_b != ROOTFIND_X_c) {
             if (fabs(ROOTFIND_X_b - ROOTFIND_X_c) <
-                EPS_TOL * (ROOTFIND_X_b + ROOTFIND_X_c))
-            {
+                EPS_TOL * (ROOTFIND_X_b + ROOTFIND_X_c)) {
                 DO_BISECTION = 1;
             }
         }
-    }
-    else
-    {
+    } else {
         if (fabs(ROOTFIND_X_new - ROOTFIND_X_b) >=
-            0.5 * fabs(ROOTFIND_X_c_old - ROOTFIND_X_c))
-        {
+            0.5 * fabs(ROOTFIND_X_c_old - ROOTFIND_X_c)) {
             DO_BISECTION = 1;
         }
-        if (ROOTFIND_X_c_old != ROOTFIND_X_c)
-        {
+        if (ROOTFIND_X_c_old != ROOTFIND_X_c) {
             if (fabs(ROOTFIND_X_c_old - ROOTFIND_X_c) <
-                EPS_TOL * (ROOTFIND_X_c_old + ROOTFIND_X_c))
-            {
+                EPS_TOL * (ROOTFIND_X_c_old + ROOTFIND_X_c)) {
                 DO_BISECTION = 1;
             }
         }
     }
-    if (DO_BISECTION)
-    {
+    if (DO_BISECTION) {
         // bisection in log space can help convergence for typical use cases; do
         // this if possible
-        if ((ROOTFIND_X_b > 0) && (ROOTFIND_X_a > 0))
-        {
+        if ((ROOTFIND_X_b > 0) && (ROOTFIND_X_a > 0)) {
             ROOTFIND_X_new = sqrt(ROOTFIND_X_b * ROOTFIND_X_a);
-        }
-        else
-        {
+        } else {
             ROOTFIND_X_new = 0.5 * (ROOTFIND_X_b + ROOTFIND_X_a);
         }
         USED_BISECTION = 1;
     } // bisection
-    else
-    {
+    else {
         USED_BISECTION = 0;
     }
     ROOTFUNC_new = ROOTFUNC(ROOTFIND_X_new);
-    if (ROOTFUNC_new == 0)
-    {
+    if (ROOTFUNC_new == 0) {
         break;
     }
 
     ROOTFIND_X_c_old = ROOTFIND_X_c;
     ROOTFIND_X_c = ROOTFIND_X_b;
     ROOTFUNC_c = ROOTFUNC_b;
-    if (ROOTFUNC_a * ROOTFUNC_new < 0)
-    {
+    if (ROOTFUNC_a * ROOTFUNC_new < 0) {
         ROOTFIND_X_b = ROOTFIND_X_new;
         ROOTFUNC_b = ROOTFUNC_new;
-    }
-    else
-    {
+    } else {
         ROOTFIND_X_a = ROOTFIND_X_new;
         ROOTFUNC_a = ROOTFUNC_new;
     }
 
-    if (fabs(ROOTFUNC_a) < fabs(ROOTFUNC_b))
-    {
+    if (fabs(ROOTFUNC_a) < fabs(ROOTFUNC_b)) {
         double tmp = ROOTFUNC_a;
         ROOTFUNC_a = ROOTFUNC_b;
         ROOTFUNC_b = tmp;
@@ -166,11 +144,11 @@ do
     }
     ROOTFIND_X_error = fabs(ROOTFIND_X_b - ROOTFIND_X_a);
     ROOTFIND_ITER++;
-    if (ROOTFIND_ITER > MAXITER)
-    {
+    if (ROOTFIND_ITER > MAXITER) {
         break;
     }
-} while (ROOTFIND_X_error > DMAX(fabs(ROOTFIND_ABS_X_tol), ROOTFIND_REL_X_tol *fabs(ROOTFIND_X_new)));
+} while (ROOTFIND_X_error > DMAX(fabs(ROOTFIND_ABS_X_tol),
+                                 ROOTFIND_REL_X_tol *fabs(ROOTFIND_X_new)));
 
 #undef ROOTFUNC
 
